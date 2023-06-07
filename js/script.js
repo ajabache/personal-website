a5d6740466bf46f0edf98853b7d8cbeaf01a1c27
Adrian Jones Abache <90118579+AdrianJones23@users.noreply.github.com>
2023-06-07 18:55:55 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+/*======typing animaiton======*/
+var typed = new Typed("#typed-text",{
+    strings:["Web Designer","Web Developer","Graphic Designer","Analyst","Programmer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+/*======aside======*/
+const nav = document.querySelector(".nav"),
+      navList = nav.querySelectorAll("li"),
+      totalNavList = navList.length;
+      for(let i=0; i<totalNavList; i++)
+      {
+        console.log()
+      }
