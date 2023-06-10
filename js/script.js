2a3733f1809c91836d241120101668338b28a6b7
Adrian Jones Abache <90118579+ajabache@users.noreply.github.com>
2023-06-10 16:57:24 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var typed = new Typed("#typed-text",{
     loop:true
 })
 /*======aside======*/
-const nav = document.querySelector(".nav"),
+const nav = document.querySelector(".nav", ".logo"),
       navList = nav.querySelectorAll("li"),
       totalNavList = navList.length,
       allSection = document.querySelectorAll(".section"),
@@ -74,3 +74,43 @@ const nav = document.querySelector(".nav"),
               allSection[i].classList.toggle("open");
             }
           }
+          function downloadFile() {
+            // Create a dummy element
+            var link = document.createElement('a');
+            link.href = 'resume.pdf'; // Relative URL to the file
+
+            // Set the file name
+            link.download = 'resume.png'; // Replace with the actual file name
+
+            // Simulate a click on the link element to trigger the download
+            link.click();
+        }
+        function openImage() {
+          document.querySelector('.overlay').classList.add('active');
+        }
+    
+        function closeImage() {
+          document.querySelector('.overlay').classList.remove('active');
+        }
+        function storeResponses() {
+          var input1Value = document.getElementById("input1").value;
+          var input2Value = document.getElementById("input2").value;
+          var input3Value = document.getElementById("input3").value;
+          var textAreaValue = document.getElementById("textArea").value;
+    
+          var storedResponses = localStorage.getItem("responses");
+          if (storedResponses) {
+            storedResponses += "\nInput 1: " + input1Value + "\nInput 2: " + input2Value + "\nInput 3: " + input3Value + "\nTextarea: " + textAreaValue + "\n";
+          } else {
+            storedResponses = "Input 1: " + input1Value + "\nInput 2: " + input2Value + "\nInput 3: " + input3Value + "\nTextarea: " + textAreaValue + "\n";
+          }
+    
+          localStorage.setItem("responses", storedResponses);
+    
+          document.getElementById("input1").value = "";
+          document.getElementById("input2").value = "";
+          document.getElementById("input3").value = "";
+          document.getElementById("textArea").value = "";
+          
+          alert("Responses stored successfully.");
+        }
